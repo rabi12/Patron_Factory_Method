73181bd9706956abf3d855442a7da96c58b7052a
rabi12 <rabi.aranda@gmail.com>
2020-01-14 01:16:34 -0600

    Patron Factory Method
    Practica

---
+++ FactoryMethod/FactoryMethod/ConcreteProduct1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class ConcreteProduct1 : IProduct
+    {
+        public string Operation()
+        {
+            return "{Result of ConcreteProduct1}";
+        }
+    }
+}
